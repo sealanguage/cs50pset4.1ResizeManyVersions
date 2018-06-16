95f6db18c3d3b9fd35156401ecd11baae9e3d8e2
Elaine Froehlich <elaine.froehlich@gmail.com>
2018-06-16 02:28:25 +0000

    pset4 resize code in progress, nothing working yet but some investigation by printing variables going.

--- resize2.c
+++ resize2.c
@@ -10,19 +10,28 @@ int main(int argc, char *argv[])
     // ensure proper usage
     if (argc != 4)
     {
-        fprintf(stderr, "Usage: copy n infile outfile\n");
+        fprintf(stderr, "Usage: resize infile outfile\n");
         return 1;
     }
 
     // remember filenames
-    char *n = argv[1];
+    // char *n = (argv[1]);
+    int n = atoi(argv[1]);
+        if(n < 1 || n > 100)
+        {
+            printf("n should be between 1 and 100\n");
+            return 1;
+        }
+
     char *infile = argv[2];
     char *outfile = argv[3];
 
-    // int size = *n;
-    // atoi(size);
-    // size <= 100;
+    int inpad = 0;
+    int outpad = 0;
 
+    // printf("char *n: %c\n", *n);
+    printf("int *inpad %d\n", inpad);
+    printf("int *outpad %d\n", outpad);
 
     // open input file
     FILE *inptr = fopen(infile, "r");
@@ -44,10 +53,14 @@ int main(int argc, char *argv[])
     // read infile's BITMAPFILEHEADER
     BITMAPFILEHEADER bf;
     fread(&bf, sizeof(BITMAPFILEHEADER), 1, inptr);
+        printf("sizeof(BITMAPFILEHEADER) %lu\n", sizeof(BITMAPFILEHEADER));
+        // printf("")
 
     // read infile's BITMAPINFOHEADER
     BITMAPINFOHEADER bi;
     fread(&bi, sizeof(BITMAPINFOHEADER), 1, inptr);
+
+
 
     // ensure infile is (likely) a 24-bit uncompressed BMP 4.0
     if (bf.bfType != 0x4d42 || bf.bfOffBits != 54 || bi.biSize != 40 ||
@@ -59,12 +72,50 @@ int main(int argc, char *argv[])
         return 4;
     }
 
+    typedef struct tagBITMAPINFOHEADER {
+        DWORD biSize;
+        LONG  biWidth;
+        LONG  biHeight;
+        WORD  biPlanes;
+        WORD  biBitCount;
+        DWORD biCompression;
+        DWORD biSizeImage;
+        LONG  biXPelsPerMeter;
+        LONG  biYPelsPerMeter;
+        DWORD biClrUsed;
+        DWORD biClrImportant;
+    } BITMAPINFOHEADER;
+
+
+    // printf("tagBITMAPINFOHEADER : %lu\n",  biWidth);
+
+    // size_t fread(void* ptr, size_t size, size_t blocks, FILE* fp);
+
+
+
     // write outfile's BITMAPFILEHEADER
     fwrite(&bf, sizeof(BITMAPFILEHEADER), 1, outptr);
 
     // write outfile's BITMAPINFOHEADER
     fwrite(&bi, sizeof(BITMAPINFOHEADER), 1, outptr);
 
+
+
+    // if(bi.biSizeImage == (bi.biWidth * bi.biHeight *3))
+    // {
+    //     // no padding needed
+    //     int padding = 0;
+    //     int padding2 = 0;
+    // }
+    // else
+    // {
+    //     // determine padding for scanlines
+    //     padding =  (4 - (bi.biWidth * n * sizeof(RGBTRIPLE)) % 4) % 4;
+    //     padding2 =  (4 - (bi.biWidth * sizeof(RGBTRIPLE)) % 4) % 4;
+    // }
+
+
+    // printf("padding and padding2 %i %i\n", padding, padding2);
 
 
     // determine padding for scanlines
@@ -84,11 +135,10 @@ int main(int argc, char *argv[])
 
             // write RGB triple to outfile
             fwrite(&triple, sizeof(RGBTRIPLE), 1, outptr);
+
+            printf("biHeight: %i\n", biHeight);
+            printf("padding: %i\n", padding);
         }
-
-
-        printf("padding: %i\n", padding);
-        // printf("positiveinteger: %d\n", n);
 
         // skip over padding, if any
         fseek(inptr, padding, SEEK_CUR);
@@ -109,3 +159,19 @@ int main(int argc, char *argv[])
     // success
     return 0;
 }
+
+
+
+
+    // int main(int argc, char* argv[])
+    // {
+    //     int padding2;
+    //     int padding;
+    //     // ensure proper usage if (argc != 4) { printf("Usage: ./resize n infile outfile\n"); return 1; }
+
+    //     int n = atoi(argv[1]);
+    //     if(n < 1 || n > 100)
+    //     {
+    //         printf("n should be between 1 and 100\n");
+    //         return 1;
+    //     }
